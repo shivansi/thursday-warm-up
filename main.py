ead681fbcf62e1ae1f86f1af9d88b885e1d8ed78
S B <barat.shivansi@gmail.com>
2020-08-06 19:38:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,62 @@
+#VARIABLE- store a piece of data
+a=2 #integer
+b="3"#string
+
+print(a+a)
+print(b+b) 
+
+#what are the 2 characteristics of data? value and data type
+
+#for loop
+for i in range(5):
+  print("hi",i)
+
+#while
+smelly=True
+while smelly==True:
+  print("idk")
+  smelly=False
+
+counter=0
+while counter<5:
+  print(counter)
+  counter+=1
+
+#1. Give me an example of a string variable and an integer variable
+#2. Create your OWN for loop
+#3. Create your OWN while loop
+#4. In a comment, provide an example of when to use:
+     # a) a variable
+     # b) a loop
+     # c) a conditionals
+#5. Provide any questions that you currently have or provide a concept you're confused about.
+
+a=8
+b="uh"
+
+for j in range(4):
+  print("hii",j)
+
+lol=False
+while lol==False:
+  print("hahaha")
+  lol=True
+
+#functions
+def greeting():
+  print("hey")
+def goodbye():
+  print("adios")
+greeting()
+goodbye()
+
+num1=input("enter one number")
+num2=input("enter another number")
+
+print(num1+num2)
+
+#a) you use a variable if you want to store a single piece of information
+#b) you use a loop when you want your code to repeat
+#c) you use conditionals when you want the computer to do a specific thing for one condition, and when you want the computer to do the opposite(?)
+
+
